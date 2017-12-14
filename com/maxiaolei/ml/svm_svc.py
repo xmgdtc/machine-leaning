bc32f1c9c7fa82fdf4e57cd18366f8abd574606f
maxiaolei <maxiaolei@shujuguan.cn>
2017-12-14 15:58:36 +0800

    增加了NuSVC和LinearSVC的样例

--- com/maxiaolei/ml/svm_svc.py
+++ com/maxiaolei/ml/svm_svc.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 
-from sklearn.svm import SVC
+from sklearn.svm import SVC,NuSVC,LinearSVC
 import numpy as np
 import matplotlib.pyplot as plt
 X= np.array([[-1,-1],[-2,-1],[1,1],[2,1]])
@@ -28,7 +28,7 @@ random_state ：数据洗牌时的种子值，int值
 主要调节的参数有：C、kernel、degree、gamma、coef0。
 '''
 
-clf = SVC(
+svc = SVC(
             C=1.0,            #C-SVC的惩罚参数C
             kernel='rbf',     #默认是rbf，可以是‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
             degree=3,         #kernel=poly的情况下有效  函数的维度，默认是3
@@ -44,11 +44,56 @@ clf = SVC(
             decision_function_shape='ovr',  #干什么用的？ ‘ovo’ 一对一, ‘ovr’ 多对多  or None 无, default=None
             random_state=None #用于概率估计的数据重排时的伪随机数生成器的种子
         )
-clf.fit(X,y)
 
+#核支持向量分类，和SVC类似，也是基于libsvm实现的，但不同的是通过一个参数空值支持向量的个数
+nuSvc = NuSVC(
+            nu=0.7,  #训练误差的一个上界和支持向量的分数的下界。应在间隔（0，1 ]。
+            kernel='rbf',
+            degree=3,
+            gamma='auto',
+            coef0=0.0,
+            shrinking=True,
+            probability=False,
+            tol=1e-3,
+            cache_size=200,
+            class_weight=None,
+            verbose=False,
+            max_iter=-1,
+            decision_function_shape='ovr',
+            random_state=None
+            )
+
+'''
+与参数kernel ='linear'的SVC类似，但是用liblinear而不是libsvm来实现，所以它在惩罚和丢失函数的选择上有更大的灵活性，并且应该更好地适应大量的样本。
+这个类同时支持密集和稀疏输入，并且多类支持是按照一种一休的方式处理的。
+'''
+linearSvc=LinearSVC(
+            penalty='l2',            #l1 l2
+            loss='squared_hinge',    #'hinge'或者'squared_hinge'指定损失函数。 'hinge'是标准的SVM损失（例如由SVC类使用），而'squared_hinge'是 'hinge'的平方。
+            dual=False,              # 优化问题。当n_samples(样本数)> n_features(属性数)时，优先使用dual = False。
+            tol=1e-4,
+            C=1.0,
+            multi_class='ovr',       #'ovr'、'crammer_singer' 多个分类时会有影响
+            fit_intercept=True,      #是否计算截距
+            intercept_scaling=1,
+            class_weight=None,
+            verbose=0,
+            random_state=None,
+            max_iter=1000
+            )
+
+
+
+svc.fit(X,y)
+nuSvc.fit(X,y)
+linearSvc.fit(X,y)
 
 #我们先来简单的预测一下
 Z=np.array([[-1,-2]])
-y1=clf.predict(Z)
+y_svc=svc.predict(Z)
+y_nuSvc=nuSvc.predict(Z)
+y_LSvc=linearSvc.predict(Z)
 #Z[0]被分到了第一类
-print(y1)
+print(y_svc)
+print(y_nuSvc)
+print(y_LSvc)
